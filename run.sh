7c186f444c7f91cefd2c365956f47ec76d61aefc
pingbowen <pingbowen23@163.com>
2024-03-21 18:33:23 +0800

    modify

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ MODEL_SAVE_DIR=save/uncalibrated_model
 mkdir -p $MODEL_SAVE_DIR
 
 CUDA_VISIBLE_DEVICES=5,6 python \
-    bitdelta/train.py \
+    bitdelta/train2.py \
     --base_model /data/public/opensource_models/meta-llama/Llama-2-7b-hf/ \
     --finetuned_model /data/public/opensource_models/WizardLM/WizardMath-7B-V1.0/ \
     --save_dir $MODEL_SAVE_DIR \
